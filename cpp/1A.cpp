8288da9e2ee69bfc3cb6ea7b831d5a79e94a839d
k2ntum <khainguyen@umass.edu>
2024-01-07 09:53:02 +0700

    [add] 1A

--- cpp/1A.cpp
+++ cpp/1A.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+long long int n, m, a;
+
 int main() {
-    std::cout << "Hello World!\n";
+    std::cin >> n >> m >> a;
+
+    auto vn = n / a;
+    auto rn = n % a;
+    auto vm = m / a;
+    auto rm = m % a;
+
+    std::cout << vn * vm + (vn * (rm > 0)) + (vm * (rn > 0)) + (rm > 0 && rn > 0) << "\n";
 }
